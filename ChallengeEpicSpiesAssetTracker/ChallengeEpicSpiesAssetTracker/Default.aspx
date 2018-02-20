6370c16ab7b8884583761fa73afb35c341073733
spencerkvarada <35581763+spencerkvarada@users.noreply.github.com>
2018-02-20 13:00:50 -0800

    committing

---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 190px;
+            height: 238px;
+        }
+    </style>
+</head>
+<body style="font-weight: 700">
+    <form id="form1" runat="server">
+        <div>
+            <img alt="" class="auto-style1" src="file:///C:/Users/Spencer/Documents/VSProjects/CS-ASP_019-Challenge/epic-spies-logo.jpg" /><br />
+            <br />
+            Asset Performance Tracker<br />
+            <br />
+            Asset Name:&nbsp;
+            <asp:TextBox ID="assetBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Elections Rigged:&nbsp;
+            <asp:TextBox ID="electionBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Acts of Subterfuge Performed:&nbsp;
+            <asp:TextBox ID="subBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Asset" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //Creating 3 separate arrays for each one.
+                //Didn't want to mess with that level of multi-dimensional arrays
+                string[] assets = new string[0];
+                int[] elections = new int[0];
+                int[] subtrefuge = new int[0];
+                //Creating viewstate entries for each array
+                ViewState.Add("Assets", assets);
+                ViewState.Add("Elections", elections);
+                ViewState.Add("Subtrefuge", subtrefuge);
+            }
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            //casting to arrays
+            string[] assets = (string[])ViewState["Assets"];
+            int[] elections = (int[])ViewState["Elections"];
+            int[] subtrefuge = (int[])ViewState["Subtrefuge"];
+            //creating a new length for each one instead of repeating myself (stolen from the solution video)
+            int newSpan = assets.Length + 1;
+            //Resizing each array to fit with the new length
+            Array.Resize(ref assets, newSpan);
+
+            Array.Resize(ref elections, newSpan);
+
+            Array.Resize(ref subtrefuge, newSpan);
+
+            //making a new index to tie all 3 arrays together
+            int useIndex = assets.GetUpperBound(0);
+            assets[useIndex] = assetBox.Text;
+            elections[useIndex] = int.Parse(electionBox.Text);
+            subtrefuge[useIndex] = int.Parse(subBox.Text);
+            //Putting everything back into viewstate
+            ViewState["Assets"] = assets;
+            ViewState["Elections"] = elections;
+            ViewState["Subtrefuge"] = subtrefuge;
+            //Making the string.format for the result label. 
+            resultLabel.Text = String.Format("Elections Rigged: {0}<br />Average Subterfuge per Asset: {1}<br />Last Asset added: {2}",
+                elections.Sum(), subtrefuge.Average(), assets[useIndex]);
+            //Clear the text boxes
+
+            assetBox.Text = "";
+            electionBox.Text = "";
+            subBox.Text = "";
+
+
+        }
+    }
+}
